867d5314c9779e4da053ce6077ae5c4f674d539f
sweatydev <strongcrood@gmail.com>
2022-12-22 13:20:12 +0100

    Added Vuetify+Tailwind[vite]

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app class="tw-bg-white tw-text-black dark:tw-bg-black dark:tw-text-white">
+    <v-main>
+      <HelloWorld  />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+  import HelloWorld from '@/components/HelloWorld.vue'
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw-',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,vue}",
+    "./src/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
